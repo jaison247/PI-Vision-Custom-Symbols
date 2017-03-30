d5adb6adcb8064d062babbccfe0f36ae8cb47eba
danielElopez <dlopez@osisoft.com>
2017-03-30 18:00:34 -0500

    Updated READMEs and fixed errors in the table symbol

--- Community Samples/OSIsoft/timeSeriesDataTable/sym-timeSeriesDataTable.js
+++ Community Samples/OSIsoft/timeSeriesDataTable/sym-timeSeriesDataTable.js
@@ -1,18 +1,22 @@
-/***************************************************************************
-   Copyright 2016 OSIsoft, LLC.
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
- ***************************************************************************/
+/**
+# ***********************************************************************
+# * DISCLAIMER:
+# *
+# * All sample code is provided by OSIsoft for illustrative purposes only.
+# * These examples have not been thoroughly tested under all conditions.
+# * OSIsoft provides no guarantee nor implies any reliability,
+# * serviceability, or function of these programs.
+# * ALL PROGRAMS CONTAINED HEREIN ARE PROVIDED TO YOU "AS IS"
+# * WITHOUT ANY WARRANTIES OF ANY KIND. ALL WARRANTIES INCLUDING
+# * THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY
+# * AND FITNESS FOR A PARTICULAR PURPOSE ARE EXPRESSLY DISCLAIMED.
+# ************************************************************************
+#
+# Updated by dlopez@osisoft.com
+#
+#
+**/
 
 //************************************
 // Begin defining a new symbol
@@ -43,10 +47,18 @@
 				// Specify the value of custom configuration options; see the "configure" section below
 				showDataItemNameCheckboxValue: true,
 				showTimestampCheckboxValue: true,
+				showDataItemNameCheckboxStyle: "table-cell",
+				showTimestampCheckboxStyle: "table-cell",				
 				numberOfDecimalPlaces: 2,
 				dataItemColumnColor: "cyan",
 				timestampColumnColor: "lightgray",
-				valueColumnColor: "lightgreen"
+				valueColumnColor: "lightgreen",
+				hoverColor: "darkslategray",
+				evenRowColor: "darkgray",
+				oddRowColor: "none",
+				outsideBorderColor: "none",
+				headerBackgroundColor: "none",
+				headerTextColor: "white",
 			};
 		},
 		// By including this, you're specifying that you want to allow configuration options for this symbol
@@ -74,14 +86,13 @@
 		this.onConfigChange = myCustomConfigurationChangeFunction;
 		
 		// Locate the html div that will contain the symbol, using its id, which is "container" by default
-		var symbolContainerDiv = elem.find('#container')[0];
+		var symbolContainerElement = elem.find('#container')[0];
         // Use random functions to generate a new unique id for this symbol, to make it unique among all other custom symbols
 		var newUniqueIDString = "myCustomSymbol_" + Math.random().toString(36).substr(2, 16);
 		// Write that new unique ID back to overwrite the old id
-        symbolContainerDiv.id = newUniqueIDString;
-		// Create a variable to hold the custom visualization object
-		var customVisualizationObject;
+        symbolContainerElement.id = newUniqueIDString;
 		var dataItemLabel = "";
+		var dataItemUnits;
 		//************************************
 		// When a data update occurs...
 		//************************************
@@ -91,50 +102,43 @@
 				// If the custom visualization hasn't been made yet... create the custom visualization!
 				// Custom code begins here:
 				// --------------------------------------------------------------------------------------------------
-				console.log("Now creating custom visualization...");
+				//console.log("Now creating custom visualization...");
 				// Clear the table
-				if(customVisualizationObject) {
-					$('#' + symbolContainerDiv.id + ' tbody').remove();
-				}
-				customVisualizationObject = true;
-				// Get the data item name
+				$('#' + symbolContainerElement.id).empty();
+
+				// Get the data item name and units
 				if (data.Data[0].Path){
 					dataItemLabel = (data.Data[0].Path.split("|"))[1]; 
+				}
+				if (data.Data[0].Units) {
+					dataItemUnits = data.Data[0].Units;
 				}
 				// For each piece of data...
 				data.Data[0].Values.forEach(function(pieceOfData) {
 					// Create a new row for the table
-					var newRow = symbolContainerDiv.insertRow(0);
-					newRow.style.width = "100%";
-					// If the "show data item" checkbox is checked, then add a cell to the row to contain this
-					if (scope.config.showDataItemNameCheckboxValue) {
-						var dataItemCell = newRow.insertCell(-1);
-						dataItemCell.innerHTML = dataItemLabel;
-						// Apply padding and the specified color for this column
-						dataItemCell.style.color = scope.config.dataItemColumnColor;
-						dataItemCell.style.paddingLeft = "10px";
-						dataItemCell.style.paddingRight = "10px";
+					var newRow = symbolContainerElement.insertRow(-1);
+					newRow.className = "myCustomRowClass";	
+					// Add a cell to the row to contain this
+					var dataItemCell = newRow.insertCell(-1);
+					dataItemCell.innerHTML = dataItemLabel;
+					dataItemCell.className = "myCustomCellClass myCustomDataItemCellClass";
+					// Add a cell to the row to contain this
+					var timeStampCell = newRow.insertCell(-1);
+					// Check to see if the timestamp is returned as a string or data object
+					if (typeof(pieceOfData.Time) == "string")
+					{
+						timeStampCell.innerHTML = (pieceOfData.Time);
+					} else {
+						timeStampCell.innerHTML = myFormatTimestampFunction(pieceOfData.Time);
 					}
-					// If the "show timestamp" checkbox is checked, then add a cell to the row to contain this
-					if (scope.config.showTimestampCheckboxValue) {
-						var timeStampCell = newRow.insertCell(-1);
-						// Check to see if the timestamp is returned as a string or data object
-						if (typeof(pieceOfData.Time) == "string")
-						{
-							timeStampCell.innerHTML = (pieceOfData.Time);
-						} else {
-							timeStampCell.innerHTML = myFormatTimestampFunction(pieceOfData.Time);
-						}
-						// Apply padding and the specified color for this column
-						timeStampCell.style.color = scope.config.timestampColumnColor;
-						timeStampCell.style.paddingRight = "10px";
-					}
+					// Apply padding and the specified color for this column
+					timeStampCell.className = "myCustomCellClass myCustomTimestampCellClass";				
 					// Add a cell to the row to contain the value
 					var valueCell = newRow.insertCell(-1);
 					var newInnerHTMLString = "";
 					// Check if the value is a string or error; if it isn't numeric, just display the raw string
 					try {
-    					newInnerHTMLString = "" + (Math.round(pieceOfData.Value * Math.pow(10, scope.config.numberOfDecimalPlaces))/Math.pow(10, scope.config.numberOfDecimalPlaces));
+    					newInnerHTMLString = parseFloat("" + pieceOfData.Value).toFixed(scope.config.numberOfDecimalPlaces);
 					}
 					catch (err) {
 						newInnerHTMLString = pieceOfData.Value;
@@ -145,33 +149,29 @@
 					}
 					valueCell.innerHTML = newInnerHTMLString;
 					// Apply padding and the specified color for this column
-					valueCell.style.color = scope.config.valueColumnColor;
-					valueCell.style.paddingRight = "10px";
+					valueCell.className = "myCustomCellClass myCustomValueCellClass";	
 				});
 				// Add a row of headers
-				var headersRow = symbolContainerDiv.insertRow(0);
-				// If the "show data item" checkbox is checked, then add a cell to the row to contain this
-				if (scope.config.showDataItemNameCheckboxValue) {
-					var dataItemHeaderCell = headersRow.insertCell(-1);
-					dataItemHeaderCell.innerHTML = "Data Item";
-					dataItemHeaderCell.style.fontWeight = "bold";
-					dataItemHeaderCell.style.paddingLeft = "10px";
-					dataItemHeaderCell.style.paddingRight = "10px";
-				}
-				// If the "show timestamp" checkbox is checked, then add a cell to the row to contain this
-				if (scope.config.showTimestampCheckboxValue) {
-					var timeStampHeaderCell = headersRow.insertCell(-1);
-					timeStampHeaderCell.innerHTML = "Timestamp";
-					timeStampHeaderCell.style.fontWeight = "bold";
-					timeStampHeaderCell.style.paddingRight = "10px";
-				}
+				var headersRow = symbolContainerElement.insertRow(0);
+				// Add a cell to the row to contain this
+				var dataItemHeaderCell = headersRow.insertCell(-1);
+				dataItemHeaderCell.innerHTML = "<b>Data Item</b>";
+				dataItemHeaderCell.className = "myCustomCellClass myCustomDataItemCellClass myCustomHeaderCellClass";					
+				// Add a cell to the row to contain this
+				var timeStampHeaderCell = headersRow.insertCell(-1);
+				timeStampHeaderCell.innerHTML = "<b>Timestamp</b>";
+				timeStampHeaderCell.className = "myCustomCellClass myCustomTimestampCellClass myCustomHeaderCellClass";		
 				// Add a cell to the row to contain the value
 				var valueHeaderCell = headersRow.insertCell(-1);
-				valueHeaderCell.innerHTML = "Value";
-				valueHeaderCell.style.fontWeight = "bold";
-				valueHeaderCell.style.paddingRight = "10px";
+				valueHeaderCell.innerHTML = "<b>Value</b>";
+				if (dataItemUnits) {
+					valueHeaderCell.innerHTML = valueHeaderCell.innerHTML + " (" + dataItemUnits + ")";
+				}
+				valueHeaderCell.style.textAlign = "right";
+				valueHeaderCell.className = "myCustomCellClass myCustomHeaderCellClass";		
 			}
 		}
+
 		//************************************
 		// Converts a date object to a small date string
 		//************************************
@@ -211,8 +211,19 @@
 		// Function that is called when custom configuration changes are made
 		//************************************
 		function myCustomConfigurationChangeFunction(data) {
-			// All configuration changes for this symbol are set up to take effect
-			// automatically every data update, so there's no need for specific config change code here
+			// Update chart background and border
+			document.getElementById(symbolContainerElement.id).style.border = "3px solid " + scope.config.outsideBorderColor;
+			// Update whether or not to show cells
+			if (scope.config.showTimestampCheckboxValue) {
+				scope.config.showTimestampCheckboxStyle = "table-cell";
+			} else {
+				scope.config.showTimestampCheckboxStyle = "none";
+			}
+			if (scope.config.showDataItemNameCheckboxValue) {
+				scope.config.showDataItemNameCheckboxStyle = "table-cell";
+			} else {
+				scope.config.showDataItemNameCheckboxStyle = "none";
+			}			
 		}
 		// Specify which function to call when a data update or configuration change occurs 
 		//return { dataUpdate: myCustomDataUpdateFunction, configChange:myCustomConfigurationChangeFunction };		
